dc6c63c2f8ea3203b7dddcfdcf3ab84ec40815ba
Constantine Molchanov <moigagoo@duck.com>
2025-12-06 00:56:25 +0400

    Impl: Intrinsics: Improve function naming.

--- src/intops/impl/intrinsics.nim
+++ src/intops/impl/intrinsics.nim
@@ -1,11 +1,11 @@
 ## Intrinsics-based implementations of arithmetic operations for integers.
 
-func overflowingAdd*[T](
+func builtinOverflowingAdd*[T](
   a, b: T, res: var T
 ): bool {.importc: "__builtin_add_overflow", nodecl.}
   ## Checks if a + b overflows. Returns true on overflow.
 
-func overflowingSub*[T](
+func builtinOverflowingSub*[T](
   a, b: T, res: var T
 ): bool {.importc: "__builtin_sub_overflow", nodecl.}
   ## Checks if a - b overflows. Returns true on overflow.
@@ -15,14 +15,14 @@ func overflowingAdd*[T: SomeUnsignedInt | SomeSignedInt](
 ): (T, bool) {.inline.} =
   var res: T
 
-  let didOverflow = overflowingAdd(a, b, res)
+  let didOverflow = builtinOverflowingAdd(a, b, res)
 
   (res, didOverflow)
 
 func saturatingAdd*[T: SomeUnsignedInt](a, b: T): T {.inline.} =
   var res: T
 
-  let didOverflow = overflowingAdd(a, b, res)
+  let didOverflow = builtinOverflowingAdd(a, b, res)
 
   if unlikely(didOverflow):
     return high(T)
@@ -35,15 +35,15 @@ func carryingAdd*[T: SomeUnsignedInt | SomeSignedInt](
   var t1, final: T
 
   let
-    c1 = intrinsics.overflowingAdd(a, b, t1)
-    c2 = intrinsics.overflowingAdd(t1, T(carryIn), final)
+    c1 = builtinOverflowingAdd(a, b, t1)
+    c2 = builtinOverflowingAdd(t1, T(carryIn), final)
 
   (final, c1 or c2)
 
 func saturatingAdd*[T: SomeSignedInt](a, b: T): T {.inline.} =
   var res: T
 
-  let didOverflow = overflowingAdd(a, b, res)
+  let didOverflow = builtinOverflowingAdd(a, b, res)
 
   if unlikely(didOverflow):
     if a < 0:
@@ -58,7 +58,7 @@ func overflowingSub*[T: SomeUnsignedInt | SomeSignedInt](
 ): (T, bool) {.inline.} =
   var res: T
 
-  let didBorrow = overflowingSub(a, b, res)
+  let didBorrow = builtinOverflowingSub(a, b, res)
 
   (res, didBorrow)
 
@@ -68,15 +68,15 @@ func borrowingSub*[T: SomeUnsignedInt | SomeSignedInt](
   var t1, final: T
 
   let
-    b1 = overflowingSub(a, b, t1)
-    b2 = overflowingSub(t1, T(borrowIn), final)
+    b1 = builtinOverflowingSub(a, b, t1)
+    b2 = builtinOverflowingSub(t1, T(borrowIn), final)
 
   (final, b1 or b2)
 
 func saturatingSub*[T: SomeUnsignedInt](a, b: T): T {.inline.} =
   var res: T
 
-  let didBorrow = overflowingSub(a, b, res)
+  let didBorrow = builtinOverflowingSub(a, b, res)
 
   if unlikely(didBorrow):
     return low(T)
@@ -86,7 +86,7 @@ func saturatingSub*[T: SomeUnsignedInt](a, b: T): T {.inline.} =
 func saturatingSub*[T: SomeSignedInt](a, b: T): T {.inline.} =
   var res: T
 
-  let didOverflow = overflowingSub(a, b, res)
+  let didOverflow = builtinOverflowingSub(a, b, res)
 
   if unlikely(didOverflow):
     if a < 0:

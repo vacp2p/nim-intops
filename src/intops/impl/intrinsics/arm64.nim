73200e71ced816a248129e057cf311ae4311575f
Constantine Molchanov <moigagoo@duck.com>
2025-12-11 00:22:46 +0400

    Attempt to fix header import.

--- src/intops/impl/intrinsics/arm64.nim
+++ src/intops/impl/intrinsics/arm64.nim
@@ -3,7 +3,7 @@
 import ../../consts
 
 when cpuArm64 and compilerGccCompatible and canUseIntrinsics:
-  {.pragma: acleheader, header: "<arm_acle.h>", nodecl, inline.}
+  {.pragma: acleheader, header: "<arm_acle.h>", nodecl.}
 
   func saturatingAdd*(a, b: uint64): uint64 {.importc: "__uqaddl", acleheader.}
 

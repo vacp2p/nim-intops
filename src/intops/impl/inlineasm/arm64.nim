12e1f358beed1b3fcae7efccd35550a4940ac2df
Constantine Molchanov <moigagoo@duck.com>
2025-12-10 22:42:12 +0400

    Fix saturating addition in ARM64 ASM.

--- src/intops/impl/inlineasm/arm64.nim
+++ src/intops/impl/inlineasm/arm64.nim
@@ -4,45 +4,29 @@ import ../../consts
 
 when cpuArm64 and compilerGccCompatible and canUseInlineAsm:
   func saturatingAdd*(a, b: uint64): uint64 {.inline.} =
-    var res: uint64
-
     asm """
-      uqadd %0, %1, %2
-      : "=r" (`res`)
-      : "r" (`a`), "r" (`b`)
+      uqadd %d0, %d1, %d2
+      : "=w" (`result`)
+      : "w" (`a`), "w" (`b`)
     """
-
-    res
 
   func saturatingAdd*(a, b: uint32): uint32 {.inline.} =
-    var res: uint32
-
     asm """
-      uqadd %w0, %w1, %w2
-      : "=r" (`res`)
-      : "r" (`a`), "r" (`b`)
+      uqadd %s0, %s1, %s2
+      : "=w" (`result`)
+      : "w" (`a`), "w" (`b`)
     """
-
-    res
 
   func saturatingAdd*(a, b: int64): int64 {.inline.} =
-    var res: int64
-
     asm """
-      sqadd %0, %1, %2
-      : "=r" (`res`)
-      : "r" (`a`), "r" (`b`)
+      sqadd %d0, %d1, %d2
+      : "=w" (`result`)
+      : "w" (`a`), "w" (`b`)
     """
-
-    res
 
   func saturatingAdd*(a, b: int32): int32 {.inline.} =
-    var res: int32
-
     asm """
-      sqadd %w0, %w1, %w2
-      : "=r" (`res`)
-      : "r" (`a`), "r" (`b`)
+      sqadd %s0, %s1, %s2
+      : "=w" (`result`)
+      : "w" (`a`), "w" (`b`)
     """
-
-    res

944c7f40895e24978ea4db3ba3c3f38a1a98b742
Constantine Molchanov <moigagoo@duck.com>
2025-12-11 00:10:21 +0400

    Add guard for arm64 definitions.

--- src/intops/impl/intrinsics/arm64.nim
+++ src/intops/impl/intrinsics/arm64.nim
@@ -1,19 +1,22 @@
 ## ARM64 intrinsics-based implementations of arithmetic operations for integers.
 
-{.pragma: acleheader, header: "<arm_acle.h>", nodecl, inline.}
+import ../../consts
 
-func saturatingAdd*(a, b: uint64): uint64 {.importc: "__uqaddl", acleheader.}
+when cpuArm64 and compilerGccCompatible and canUseIntrinsics:
+  {.pragma: acleheader, header: "<arm_acle.h>", nodecl, inline.}
 
-func saturatingAdd*(a, b: uint32): uint32 {.importc: "__uqadd", acleheader.}
+  func saturatingAdd*(a, b: uint64): uint64 {.importc: "__uqaddl", acleheader.}
 
-func saturatingAdd*(a, b: int64): int64 {.importc: "__sqaddl", acleheader.}
+  func saturatingAdd*(a, b: uint32): uint32 {.importc: "__uqadd", acleheader.}
 
-func saturatingAdd*(a, b: int32): int32 {.importc: "__sqadd", acleheader.}
+  func saturatingAdd*(a, b: int64): int64 {.importc: "__sqaddl", acleheader.}
 
-func saturatingSub*(a, b: int64): int64 {.importc: "__sqsubl", acleheader.}
+  func saturatingAdd*(a, b: int32): int32 {.importc: "__sqadd", acleheader.}
 
-func saturatingSub*(a, b: int32): int32 {.importc: "__sqsub", acleheader.}
+  func saturatingSub*(a, b: int64): int64 {.importc: "__sqsubl", acleheader.}
 
-func saturatingSub*(a, b: uint64): uint64 {.importc: "__uqsubl", acleheader.}
+  func saturatingSub*(a, b: int32): int32 {.importc: "__sqsub", acleheader.}
 
-func saturatingSub*(a, b: uint32): uint32 {.importc: "__uqsub", acleheader.}
+  func saturatingSub*(a, b: uint64): uint64 {.importc: "__uqsubl", acleheader.}
+
+  func saturatingSub*(a, b: uint32): uint32 {.importc: "__uqsub", acleheader.}

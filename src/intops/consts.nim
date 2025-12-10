bf7d8d97d79f7760ab69a88dd379570250fe6700
Constantine Molchanov <moigagoo@duck.com>
2025-12-10 21:24:37 +0400

    Format constants.

--- src/intops/consts.nim
+++ src/intops/consts.nim
@@ -1,11 +1,11 @@
 ## CPU and compiler detection constants and compilation flags passed by the user.
 
 const
-  cpuX86* = defined(amd64) or defined(i386) ## Is the current CPU an x86 one.
-  cpu64Bit* = sizeof(int) == 8 ## Is the current CPU 64 bit.
+  cpuX86* = defined(amd64) or defined(i386) ## Is the current CPU an x86 one?
+  cpuArm64* = defined(arm64) or defined(aarch64) ## Is the current CPU an ARM64?
+  cpu64Bit* = sizeof(int) == 8 ## Is the current CPU 64 bit?
   compilerGccCompatible* = defined(gcc) or defined(clang) or defined(llvm_gcc)
-    ## Is the current C compiler compatible with GCC.
-  noIntrinsics* = defined(intopsNoIntrinsics)
-    ## Forbid usage of C intrinsics implementations.
-  noInlineAsm* = defined(intopsNoInlineAsm) ## Forbid inline Assembly implementations.
-  noInlineC* = defined(intopsNoInlineC) ## Forbid inline C implenentations.
+    ## Is the current C compiler compatible with GCC?
+  canUseIntrinsics* = not defined(intopsNoIntrinsics) ## C intrinsics are not forbidden.
+  canUseInlineAsm* = not defined(intopsNoInlineAsm) ## Inline Assembly is not forbidden.
+  canUseInlineC* = not defined(intopsNoInlineC) ## Inline C is not forbidden.

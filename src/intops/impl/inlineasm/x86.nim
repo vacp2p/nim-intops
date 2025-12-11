b7d5c62a4e49fd13bab77193969070a35d139659
Constantine Molchanov <moigagoo@duck.com>
2025-12-11 16:00:12 +0400

    Sub: Add x86 ASM implementation for uint64.

--- src/intops/impl/inlineasm/x86.nim
+++ src/intops/impl/inlineasm/x86.nim
@@ -13,7 +13,7 @@ when cpu64Bit and cpuX86 and compilerGccCompatible and canUseInlineAsm:
       negq %2      /* Sets CF=1 if cInVal==1, CF=0 if cInVal==0 */
       adcq %3, %0  /* sum = sum + b + CF */
       setc %1      /* Store Carry Flag (CF) into cOut */
-    
+
       : "+r"(`sum`), "=q"(`cOut`), "+r"(`cInVal`)
       : "r"(`b`)
       : "cc"
@@ -30,12 +30,46 @@ when cpu64Bit and cpuX86 and compilerGccCompatible and canUseInlineAsm:
       negq %2      /* Sets CF based on carryIn (Standard trick) */
       adcq %3, %0  /* Signed addition is binary-identical to unsigned */
       seto %1      /* Check OVERFLOW Flag (OF) instead of Carry */
-    
+
       : "+r"(`sum`), "=q"(`didOverflow`), "+r"(`cInVal`)
       : "r"(`b`)
       : "cc"
     """
     (sum, didOverflow > 0)
+
+  func borrowingSub*(a, b: uint64, borrowIn: bool): (uint64, bool) {.inline.} =
+    var
+      diff = a
+      bOut: uint8
+      bInVal = if borrowIn: 1'u64 else: 0'u64
+
+    asm """
+      negq %2      /* Sets CF=1 if bInVal==1, CF=0 if bInVal==0 */
+      sbbq %3, %0  /* diff = diff - b - CF */
+      setc %1      /* Store Carry Flag (CF) into bOut */
+
+      : "+r"(`diff`), "=q"(`bOut`), "+r"(`bInVal`)
+      : "r"(`b`)
+      : "cc"
+    """
+    (diff, bOut > 0)
+
+  func borrowingSub*(a, b: int64, borrowIn: bool): (int64, bool) {.inline.} =
+    var
+      diff = a
+      didOverflow: uint8
+      bInVal = if borrowIn: 1'u64 else: 0'u64
+
+    asm """
+      negq %2      /* Prime CF based on borrowIn */
+      sbbq %3, %0  /* Signed subtraction is binary-identical to unsigned */
+      seto %1      /* Check OVERFLOW Flag (OF) for signed result validity */
+
+      : "+r"(`diff`), "=q"(`didOverflow`), "+r"(`bInVal`)
+      : "r"(`b`)
+      : "cc"
+    """
+    (diff, didOverflow > 0)
 
 when cpuX86 and compilerGccCompatible and canUseInlineAsm:
   func carryingAdd*(a, b: uint32, carryIn: bool): (uint32, bool) {.inline.} =
@@ -48,7 +82,7 @@ when cpuX86 and compilerGccCompatible and canUseInlineAsm:
       negl %2      /* 32-bit Negate */
       adcl %3, %0  /* 32-bit Add with Carry */
       setc %1      /* Store Carry Flag */
-    
+
       : "+r"(`sum`), "=q"(`cOut`), "+r"(`cInVal`)
       : "r"(`b`)
       : "cc"
@@ -65,9 +99,43 @@ when cpuX86 and compilerGccCompatible and canUseInlineAsm:
       negl %2
       adcl %3, %0
       seto %1      /* Check OVERFLOW Flag (OF) */
-    
+
       : "+r"(`sum`), "=q"(`didOverflow`), "+r"(`cInVal`)
       : "r"(`b`)
       : "cc"
     """
     (sum, didOverflow > 0)
+
+  func borrowingSub*(a, b: uint32, borrowIn: bool): (uint32, bool) {.inline.} =
+    var
+      diff = a
+      bOut: uint8
+      bInVal = if borrowIn: 1'u32 else: 0'u32
+
+    asm """
+      negl %2      /* 32-bit Negate to set CF */
+      sbbl %3, %0  /* 32-bit Subtract with Borrow */
+      setc %1      /* Store Carry Flag */
+
+      : "+r"(`diff`), "=q"(`bOut`), "+r"(`bInVal`)
+      : "r"(`b`)
+      : "cc"
+    """
+    (diff, bOut > 0)
+
+  func borrowingSub*(a, b: int32, borrowIn: bool): (int32, bool) {.inline.} =
+    var
+      diff = a
+      didOverflow: uint8
+      bInVal = if borrowIn: 1'u32 else: 0'u32
+
+    asm """
+      negl %2
+      sbbl %3, %0
+      seto %1      /* Check OVERFLOW Flag (OF) */
+
+      : "+r"(`diff`), "=q"(`didOverflow`), "+r"(`bInVal`)
+      : "r"(`b`)
+      : "cc"
+    """
+    (diff, didOverflow > 0)
--- src/intops/ops/sub.nim
+++ src/intops/ops/sub.nim
@@ -42,8 +42,71 @@ template saturatingSub*[T: SomeInteger](a, b: T): T =
     else:
       pure.saturatingSub(a, b)
 
-template borrowingSub*[T: SomeInteger](a, b: T, borrowIn: bool): (T, bool) =
-  ##[ Borrowing subtraction.
+template borrowingSub*(a, b: uint64, borrowIn: bool): (uint64, bool) =
+  ##[ Borrowing subtraction for unsigned 64-bit integers.
+
+  Takes two integers and returns their difference along with the borrow flag (BF): 
+  ``true`` means the previous subtraction had overflown, ``false`` means it hadn't.
+
+  Useful for chaining operations.
+
+  See also:
+  - `borrowingSub`_
+  ]##
+
+  when nimvm:
+    pure.borrowingSub(a, b, borrowIn)
+  else:
+    when cpu64Bit and cpuX86 and compilerGccCompatible and canUseInlineAsm:
+      # Use inline ASM for Linux/Mac on x86 x64
+      inlineasm.x86.borrowingSub(a, b, borrowIn)
+    elif compilerGccCompatible and canUseIntrinsics:
+      intrinsics.gcc.borrowingSub(a, b, borrowIn)
+    else:
+      pure.borrowingSub(a, b, borrowIn)
+
+template borrowingSub*(a, b: uint32, borrowIn: bool): (uint32, bool) =
+  ##[ Borrowing subtraction for unsigned 32-bit integers.
+
+  Takes two integers and returns their difference along with the borrow flag (BF): 
+  ``true`` means the previous subtraction had overflown, ``false`` means it hadn't.
+
+  Useful for chaining operations.
+
+  See also:
+  - `borrowingSub`_
+  ]##
+
+  when nimvm:
+    pure.borrowingSub(a, b, borrowIn)
+  else:
+    when compilerGccCompatible and canUseIntrinsics:
+      intrinsics.gcc.borrowingSub(a, b, borrowIn)
+    else:
+      pure.borrowingSub(a, b, borrowIn)
+
+template borrowingSub*(a, b: int64, borrowIn: bool): (int64, bool) =
+  ##[ Borrowing subtraction for signed 64-bit integers.
+
+  Takes two integers and returns their difference along with the borrow flag (BF):
+  ``true`` means the previous subtraction had overflown, ``false`` means it hadn't.
+
+  Useful for chaining operations.
+
+  See also:
+  - `borrowingSub`_
+  ]##
+
+  when nimvm:
+    pure.borrowingSub(a, b, borrowIn)
+  else:
+    when compilerGccCompatible and canUseIntrinsics:
+      intrinsics.gcc.borrowingSub(a, b, borrowIn)
+    else:
+      pure.borrowingSub(a, b, borrowIn)
+
+template borrowingSub*(a, b: int32, borrowIn: bool): (int32, bool) =
+  ##[ Borrowing subtraction for signed 32-bit integers.
 
   Takes two integers and returns their difference along with the borrow flag (BF): 
   ``true`` means the previous subtraction had overflown, ``false`` means it hadn't.

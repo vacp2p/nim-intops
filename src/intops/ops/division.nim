0b6431aff6c3a3ec0ef107b30061710c29ff52e4
Constantine Molchanov <moigagoo@duck.com>
2025-12-13 18:46:41 +0400

    Div: Use intrinsics version on MSVC.

--- src/intops/ops/division.nim
+++ src/intops/ops/division.nim
@@ -1,4 +1,4 @@
-import ../impl/[pure, inlinec]
+import ../impl/[pure, intrinsics, inlinec]
 
 import ../consts
 
@@ -7,7 +7,7 @@ template narrowingDiv*(uHi, uLo, v: uint64): tuple[q, r: uint64] =
 
   Takes three unsigned 64-bit integers: the dividend high word, the dividend low word, and the divisor.
 
-  Returns the quontient and the remainder.
+  Returns the quotient and the remainder.
   ]##
 
   when nimvm:
@@ -15,5 +15,7 @@ template narrowingDiv*(uHi, uLo, v: uint64): tuple[q, r: uint64] =
   else:
     when cpu64Bit and compilerGccCompatible and canUseInlineC:
       inlinec.narrowingDiv(uHi, uLo, v)
+    elif cpu64Bit and cpuX86 and compilerMsvc and canUseIntrinsics:
+      intrinsics.x86.narrowingDiv(uHi, uLo, v)
     else:
       pure.narrowingDiv(uHi, uLo, v)

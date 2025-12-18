eca3e83aa7e8013956ad33e6f9fa64ab1118e155
Constantine Molchanov <moigagoo@duck.com>
2025-12-18 16:19:04 +0400

    Book: Rename Quick Start to Quickstart.

--- nbook.nim
+++ nbook.nim
@@ -2,7 +2,7 @@ import nimibook
 
 var book = initBookWithToc:
   entry("Welcome to intops!", "index.nim")
-  entry("Quick Start", "quickstart.nim")
+  entry("Quickstart", "quickstart.nim")
   section("Contributor's Guide", "contrib.nim"):
     entry("Improve dispatching", "contrib/dispatch.nim")
     entry("Add new operations", "contrib/ops.nim")

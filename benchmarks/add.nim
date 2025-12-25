3e9755531afe165a70940be27794508b189d16ad
Constantine Molchanov <moigagoo@duck.com>
2025-12-26 00:30:14 +0400

    Benchmarks: Factor out the harness code into a separate module.

--- benchmarks/add.nim
+++ benchmarks/add.nim
@@ -1,81 +1,29 @@
-import std/[random, monotimes, times, strformat]
+import std/strutils
 
 import intops/impl/[pure, intrinsics, inlinec, inlineasm]
 
 import benchutils
 
-const
-  iterations = 100_000_000
-  bufSize = 1024
+template benchCarrying(typ: typedesc, op: untyped) =
+  let opName = astToStr(op)
 
-template benchmarkLatencyCarryingAdd(typ: typedesc, opName: untyped) =
-  block:
-    var
-      randGen = initRand()
-      inputsA = newSeq[typ](bufSize)
-      inputsB = newSeq[typ](bufSize)
-      sink: typ
-      carryIn: bool
+  when not compiles op(default(typ), default(typ), false):
+    echo alignLeft(opName, 35), " -"
+  elif typeof(op(default(typ), default(typ), false)[0]) isnot typ:
+    echo alignLeft(opName, 35), " -"
+  else:
+    measureLatency(typ, opName):
+      var carryIn {.inject.}: bool
+    do:
+      let (res, carryOut) = op(inputsA[idx], inputsB[idx], carryIn)
 
-    for i in 0 ..< bufSize:
-      inputsA[i] = typ(randGen.next())
-      inputsB[i] = typ(randGen.next())
-
-    for i in 0 ..< 1000:
-      let
-        idx = i and (bufSize - 1)
-        (res, _) = opName(inputsA[idx], inputsB[idx], sink > 0)
-
-      sink = res
-
-    let timeStart = getMonoTime()
-    for i in 0 ..< iterations:
-      let
-        idx = i and (bufSize - 1)
-        (res, carryOut) = opName(inputsA[idx], inputsB[idx], carryIn)
-
-      sink = res
+      flush = res
       carryIn = carryOut
 
-    let timeFinish = getMonoTime()
-
-    doNotOptimize(sink)
-
-    let
-      timeDelta = float64((timeFinish - timeStart).inNanoseconds)
-      nanoSecsPerOp {.inject.} = timeDelta / float64(iterations)
-      opNameStr {.inject.} = astToStr(opName)
-
-    echo fmt"{opNameStr:<30} {nanoSecsPerOp}"
-
-template benchmarkCarryingAdd(typ: typedesc, opName: untyped) =
-  block benchmark:
-    when not compiles opName(default(typ), default(typ), false):
-      let opNameStr {.inject.} = astToStr(opName)
-      echo &"{opNameStr:<30} -"
-      break benchmark
-    else:
-      when typeof(opName(default(typ), default(typ), false)) isnot (typ, bool):
-        let opNameStr {.inject.} = astToStr(opName)
-        echo &"{opNameStr:<30} -"
-        break benchmark
-      else:
-        benchmarkLatencyCarryingAdd(typ, opName)
-
-echo ""
-echo "=== Carrying Add, uint64 ==="
-benchmarkCarryingAdd(uint64, pure.carryingAdd)
-benchmarkCarryingAdd(uint64, intrinsics.x86.carryingAdd)
-benchmarkCarryingAdd(uint64, intrinsics.gcc.carryingAdd)
-benchmarkCarryingAdd(uint64, inlinec.carryingAdd)
-benchmarkCarryingAdd(uint64, inlineasm.x86.carryingAdd)
-benchmarkCarryingAdd(uint64, inlineasm.arm64.carryingAdd)
-
-echo ""
-echo "=== Carrying Add, uint32 ==="
-benchmarkCarryingAdd(uint32, pure.carryingAdd)
-benchmarkCarryingAdd(uint32, intrinsics.x86.carryingAdd)
-benchmarkCarryingAdd(uint32, intrinsics.gcc.carryingAdd)
-benchmarkCarryingAdd(uint32, inlinec.carryingAdd)
-benchmarkCarryingAdd(uint32, inlineasm.x86.carryingAdd)
-benchmarkCarryingAdd(uint32, inlineasm.arm64.carryingAdd)
+echo "\n=== Carrying Add, uint64 ==="
+benchCarrying(uint64, pure.carryingAdd)
+benchCarrying(uint64, intrinsics.x86.carryingAdd)
+benchCarrying(uint64, intrinsics.gcc.carryingAdd)
+benchCarrying(uint64, inlinec.carryingAdd)
+benchCarrying(uint64, inlineasm.x86.carryingAdd)
+benchCarrying(uint64, inlineasm.arm64.carryingAdd)
--- benchmarks/benchutils.nim
+++ benchmarks/benchutils.nim
@@ -1,3 +1,9 @@
+import std/[random, monotimes, times, strutils]
+
+const
+  iterations* = 100_000_000
+  bufSize* = 1024
+
 func doNotOptimize*[T](x: var T) {.inline.} =
   ## Force the compiler to avoid optimizing away a variable.
 
@@ -5,3 +11,46 @@ func doNotOptimize*[T](x: var T) {.inline.} =
     {.emit: """asm volatile("" : "+g"(`x`) : : "memory");""".}
   else:
     var volatileSink {.volatile.}: T = x
+
+template measureLatency*(typ: typedesc, opName: string, setupBlock, loopBlock: untyped) =
+  ##[ Universal latency measurement template. Provides random test sets of the given type,
+  warms up the CPU before the benchmark, measures the laetncy (nanoseconds per operation),
+  and prints the result.
+
+  Injected variables can be used in `setupBlock` and `loopBlock`.
+
+  The user can define new injected variables in `setupBlock` that can then be used in `loopBlock`.
+  ]##
+
+  block:
+    var
+      randGen = initRand(123)
+      flush {.inject.}: typ
+      inputsA {.inject.}: array[bufSize, typ]
+      inputsB {.inject.}: array[bufSize, typ]
+
+    for i in 0 ..< bufSize:
+      inputsA[i] = typ(randGen.next())
+      inputsB[i] = typ(randGen.next())
+
+    setupBlock
+
+    for i in 0 ..< 1000:
+      let idx {.inject.} = i and (bufSize - 1)
+      loopBlock
+
+    let timeStart = getMonoTime()
+
+    for i in 0 ..< iterations:
+      let idx {.inject.} = i and (bufSize - 1)
+      loopBlock
+
+    let timeFinish = getMonoTime()
+
+    doNotOptimize(flush)
+
+    let
+      timeDelta = (timeFinish - timeStart).inNanoseconds
+      nanosecsPerOp = float64(timeDelta) / float64(iterations)
+
+    echo alignLeft(opName, 35), " ", formatFloat(nanosecsPerOp, ffDecimal, 3)

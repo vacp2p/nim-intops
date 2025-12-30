afebeb53d105e047b06023712f2df8d0f22d2629
Constantine Molchanov <moigagoo@duck.com>
2025-12-30 14:54:42 +0400

    Bench: Move repeating code into a separate utils module.

--- benchmarks/latency/add.nim
+++ benchmarks/latency/add.nim
@@ -1,67 +1,6 @@
-import std/strutils
-
 import intops/impl/[pure, intrinsics, inlinec, inlineasm]
 
-import ../utils
-
-template benchLatencyOverflowing(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ)):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ))[0]) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureLatency(typ, opName):
-      var
-        currentA {.inject.} = inputsA[0]
-        valFlush {.inject.}: typ
-        ovfFlush {.inject.}: bool
-    do:
-      let (res, didOverflow) = op(currentA, inputsB[idx])
-      ovfFlush = ovfFlush xor didOverflow
-      currentA = res xor typ(ovfFlush)
-      valFlush = currentA
-    do:
-      doNotOptimize(valFlush)
-      doNotOptimize(ovfFlush)
-
-template benchLatencySaturating(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ)):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ))) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureLatency(typ, opName):
-      var
-        flush {.inject.}: typ
-        currentA {.inject.} = inputsA[0]
-    do:
-      currentA = op(currentA, inputsB[idx])
-      flush = currentA
-    do:
-      doNotOptimize(flush)
-
-template benchLatencyCarrying(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ), false):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ), false)[0]) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureLatency(typ, opName):
-      var
-        flush {.inject.}: typ
-        carryIn {.inject.}: bool
-    do:
-      let (res, carryOut) = op(inputsA[idx], inputsB[idx], carryIn)
-      flush = res
-      carryIn = carryOut
-    do:
-      doNotOptimize(flush)
+import utils, ../utils.nim
 
 proc runLatencyOverflowing() {.noinline.} =
   benchTypesAndImpls(benchLatencyOverflowing, overflowingAdd)
--- benchmarks/throughput/sub.nim
+++ benchmarks/throughput/sub.nim
@@ -1,56 +1,6 @@
-import std/strutils
-
 import intops/impl/[pure, intrinsics, inlinec, inlineasm]
 
-import ../utils
-
-template benchThroughputOverflowing(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ)):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ))[0]) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureThroughput(typ, opName):
-      var flush {.inject.}: typ
-    do:
-      let (res, didOverflow) = op(inputsA[idx], inputsB[idx])
-      flush = flush xor res xor cast[typ](didOverflow)
-    do:
-      doNotOptimize(flush)
-
-template benchThroughputSaturating(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ)):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ))) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureThroughput(typ, opName):
-      var flush {.inject.}: typ
-    do:
-      let res = op(inputsA[idx], inputsB[idx])
-      flush = flush xor res
-    do:
-      doNotOptimize(flush)
-
-template benchThroughputCarrying(typ: typedesc, op: untyped) =
-  let opName = astToStr(op)
-
-  when not compiles op(default(typ), default(typ), false):
-    echo alignLeft(opName, 35), " -"
-  elif typeof(op(default(typ), default(typ), false)[0]) isnot typ:
-    echo alignLeft(opName, 35), " -"
-  else:
-    measureThroughput(typ, opName):
-      var flush {.inject.}: typ
-    do:
-      let (res, _) = op(inputsA[idx], inputsB[idx], boolInputs[idx])
-      flush = flush xor res
-    do:
-      doNotOptimize(flush)
+import utils, ../utils
 
 proc runThroughputOverflowing() {.noinline.} =
   benchTypesAndImpls(benchThroughputOverflowing, overflowingSub)
@@ -67,4 +17,3 @@ when isMainModule:
   runThroughputOverflowing()
   runThroughputSaturating()
   runThroughputCarrying()
-

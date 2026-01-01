2b7679ba4de4f64149e43bb94becab19db7a5bb9
Constantine Molchanov <moigagoo@duck.com>
2026-01-01 21:16:13 +0400

    Fix typo.

--- src/intops/ops/composite.nim
+++ src/intops/ops/composite.nim
@@ -2,8 +2,9 @@ import ../impl/pure
 
 template mulDoubleAdd2*[T: uint64 | uint32](
     a, b, c, dHi, dLo: T
-): tuple[t2, r1, r0: T] =
+): tuple[r2, r1, r0: T] =
   ##[ Calculates: (r2, r1, r0) = 2 * a * b + c + (dHi, dLo)
+
   Returns (r2, r1, r0) where r2 is the overflow carry (0 or 1).
   ]##
 
@@ -11,6 +12,7 @@ template mulDoubleAdd2*[T: uint64 | uint32](
 
 func mulAcc*[T: uint64 | uint32](t, u, v: T, a, b: T): tuple[t, u, v: T] =
   ##[ Calculates: (t, u, v) <- (t, u, v) + a * b
+
   Used for Comba multiplication column accumulation.
   ]##
 

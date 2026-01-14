f03f55feabb1fbfa4d1864bf14edd5c3a4259ca1
Constantine Molchanov <moigagoo@duck.com>
2026-01-14 17:30:15 +0400

    Add full stop to improve docs readbility.

--- src/intops/ops/composite.nim
+++ src/intops/ops/composite.nim
@@ -12,7 +12,7 @@ import ../impl/pure
 template mulDoubleAdd2*[T: uint64 | uint32](
     a, b, c, dHi, dLo: T
 ): tuple[r2, r1, r0: T] =
-  ##[ Calculates: (r2, r1, r0) = 2 * a * b + c + (dHi, dLo)
+  ##[ Calculates: (r2, r1, r0) = 2 * a * b + c + (dHi, dLo).
 
   Returns (r2, r1, r0) where r2 is the overflow carry (0 or 1).
   ]##
@@ -20,7 +20,7 @@ template mulDoubleAdd2*[T: uint64 | uint32](
   pure.mulDoubleAdd2(a, b, c, dHi, dLo)
 
 func mulAcc*[T: uint64 | uint32](t, u, v, a, b: T): tuple[t, u, v: T] =
-  ##[ Calculates: (t, u, v) <- (t, u, v) + a * b
+  ##[ Calculates: (t, u, v) <- (t, u, v) + a * b.
 
   Used for Comba multiplication column accumulation.
   ]##

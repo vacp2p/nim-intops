76e505b3208d5ad9c78a3c2a4e684688a1fd47ec
Constantine Molchanov <moigagoo@duck.com>
2026-01-19 14:40:34 +0400

    Move composite ops implementations to the dispatchers.

--- src/intops/ops/composite.nim
+++ src/intops/ops/composite.nim
@@ -7,7 +7,7 @@
 #
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
-import ../impl/pure
+import ../../intops
 
 template mulDoubleAdd2*[T: uint64 | uint32](
     a, b, c, dHi, dLo: T
@@ -16,11 +16,43 @@ template mulDoubleAdd2*[T: uint64 | uint32](
   Returns (r2, r1, r0) where r2 is the overflow carry (0 or 1).
   ]##
 
-  pure.mulDoubleAdd2(a, b, c, dHi, dLo)
+  var (r1, r0) = wideningMul(a, b)
 
-func mulAcc*[T: uint64 | uint32](t, u, v: T, a, b: T): tuple[t, u, v: T] =
+  let (r0_new, c1) = carryingAdd(r0, r0, false)
+  r0 = r0_new
+
+  let (r1_new, c2) = carryingAdd(r1, r1, c1)
+  r1 = r1_new
+
+  var r2 = T(c2)
+
+  let
+    (sum0, c3) = carryingAdd(r0, c, false)
+    (sum1, c4) = carryingAdd(r1, T(0), c3)
+
+  r0 = sum0
+  r1 = sum1
+  r2 += T(c4)
+
+  let
+    (final0, c5) = carryingAdd(r0, dLo, false)
+    (final1, c6) = carryingAdd(r1, dHi, c5)
+
+  r0 = final0
+  r1 = final1
+  r2 += T(c6)
+
+  (r2, r1, r0)
+
+template mulAcc*[T: uint64 | uint32](t, u, v: T, a, b: T): tuple[t, u, v: T] =
   ##[ Calculates: (t, u, v) <- (t, u, v) + a * b
   Used for Comba multiplication column accumulation.
   ]##
 
-  pure.mulAcc(t, u, v, a, b)
+  let
+    (pHi, pLo) = wideningMul(a, b)
+    (newV, carry1) = carryingAdd(v, pLo, false)
+    (newU, carry2) = carryingAdd(u, pHi, carry1)
+    newT = t + T(carry2)
+
+  (newT, newU, newV)

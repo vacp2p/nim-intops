5eb104ed523a29f154d1d7b705711973a0ca4953
moigagoo <moigagoo@users.noreply.github.com>
2026-03-26 10:33:25 +0000

    deploy: f1b4b242beef6e2458716ea09fbdb96159e2d362

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-91a32e5d.js',
+                path_to_root + 'searchindex-b9a5591d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
